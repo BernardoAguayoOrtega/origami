623446a72b5ba715456cf34ad9d0838bd4445f8a
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-12 21:54:26 -0600

    [add] add metadata

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 function HomePage(){
   return (
     <>
+    <Head>
+      <title>Welcome</title>
+    </Head>
       <div>Welcome to Next.js!</div>
       <Link href='/posts/first'>
         <a>First Post</a>
--- pages/posts/first.js
+++ pages/posts/first.js
@@ -1,16 +1,20 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Head from 'next/head';
 
 export default function FirstPost() {
 	return (
 		<>
+			<Head>
+				<title>My first post</title>
+			</Head>
 			<h1>My first post</h1>
-      <h2>
-        <Link href='/'>
-          <a>Home</a>
-        </Link>
-      </h2>
-      <br/>
-      <img src='msdlas' alt='some image'/>
+			<h2>
+				<Link href='/'>
+					<a>Home</a>
+				</Link>
+			</h2>
+			<br />
+			<img src='msdlas' alt='some image' />
 		</>
 	);
 }
