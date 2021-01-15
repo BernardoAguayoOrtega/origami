1ee45133711a2777a4e43e3a51a935c0ad71a1b2
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-15 16:33:14 -0600

    [add] add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,22 @@ function HomePage() {
 				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
 			</Head>
 			<Container style='home'>
-				home
+				<div className='title'>
+					<h1>Origami, "Ori" means folding and</h1>
+					<h1>"Gami" means paper.</h1>
+				</div>
 			</Container>
+			<style jsx>{`
+				.title {
+					margin-left: 3rem;
+					margin-top: 3rem;
+				}
+
+				h1 {
+					color: rgba(0,0,0,.7);
+					font-weight: 700;
+				}
+			`}</style>
 		</>
 	);
 }
