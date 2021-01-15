f815029580c2896b5bb39808da970172cc7b61fb
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-15 16:06:58 -0600

    [finish] finish header component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,27 @@
+import Link from 'next/link';
 import { ReactElement } from 'react';
 import styles from './header.module.css';
+import { GiFoldedPaper } from 'react-icons/gi';
 
 const Header = (): ReactElement => {
 	return (
-		<>
-			<div className={styles.header}>I'm the header</div>
-		</>
+		<div className={styles.header}>
+			<GiFoldedPaper className={styles.icon} />
+			<div className={styles.linksContainer}>
+				<Link href='/'>
+					<a className={styles.link}>tutorials</a>
+				</Link>
+				<Link href='/'>
+					<a className={styles.link}>about</a>
+				</Link>
+				<Link href='/'>
+					<a className={styles.link}>contact</a>
+				</Link>
+				<Link href='/'>
+					<a className={styles.subscribe}>subscribe</a>
+				</Link>
+			</div>
+		</div>
 	);
 };
 
