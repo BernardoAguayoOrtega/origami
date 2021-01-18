e33b19b5df9ee7e5b6ed8673a13a28bab2484bc6
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-18 16:16:23 -0600

    [update] update styles

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -27,12 +27,6 @@ const Carousel: React.FC = (): ReactElement => {
 			<Card title='test' image='https://images.unsplash.com/photo-1591808338461-a544265cfb09?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8b3JpZ2FtaXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' description='lorem'>
 				Card
 			</Card>
-			<Card title='test' image='https://images.unsplash.com/photo-1591808338461-a544265cfb09?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8b3JpZ2FtaXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' description='lorem'>
-				Card
-			</Card>
-			<Card title='test' image='https://images.unsplash.com/photo-1591808338461-a544265cfb09?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8b3JpZ2FtaXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' description='lorem'>
-				Card
-			</Card>
 		</div>
 	)
 };
